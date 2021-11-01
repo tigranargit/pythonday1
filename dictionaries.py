a70d70cde8b27a4c407256ca13ab650ef001a388
tigrank <karapetyan.t@gmail.com>
2021-11-01 16:21:56 +0000

    third commut

---
+++ dictionaries.py
@@ -0,0 +1,3 @@
+books = {"The Handmaiden's Tale": "Margaret Atwood", "The Hobbit": "J.R.R. Tolkien", "Charlie and the Chocoalate Factory":"Roald Dahl"}
+print(books["The Handmaiden's Tale"])
+print(books["Margaret Atwood"])
