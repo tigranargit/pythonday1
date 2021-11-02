7e22192920cadc79a5bc8f19d08b5653f3a61e25
tigrank <karapetyan.t@gmail.com>
2021-11-02 12:08:42 +0000

    names exercise

---
+++ loops.py
@@ -0,0 +1,28 @@
+
+#name1=str(input("Enter first name:"))
+#name2=str(input("Enter second name:"))
+#name3=str(input("Enter third name:"))
+#name4=str(input("Enter fourth name:"))
+#name5=str(input("Enter fifth name:"))
+
+#names=[name1,name2, name3, name4, name5]
+
+#for name in names:
+#    print(name, "is awesome!")
+
+#for i in range(10, 21, 2):
+#   print(i)
+
+name1=str(input("Enter first name:"))
+name2=str(input("Enter second name:"))
+name3=str(input("Enter third name:"))
+name4=str(input("Enter fourth name:"))
+name5=str(input("Enter fifth name:"))
+
+names=[name1,name2, name3, name4, name5]
+count = 0
+
+while count < 5:
+    
+     print(names[count], "is awesome")
+     count +=1
